ca16f50cb06e012ba0582365923331483f71f3e9
Adam Lee <adam@codelabx.ltd>
2022-10-26 16:14:31 +0100

    Filament Brands Table

--- src/Admin/Livewire/Components/Brands/BrandsTable.php
+++ src/Admin/Livewire/Components/Brands/BrandsTable.php
@@ -2,49 +2,64 @@
 
 namespace Xtend\Extensions\Lunar\Admin\Livewire\Components\Brands;
 
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
 use Lunar\Hub\Http\Livewire\Traits\Notifies;
-use Lunar\Hub\Tables\LunarTable;
-use Lunar\LivewireTables\Components\Columns\ImageColumn;
-use Lunar\LivewireTables\Components\Columns\TextColumn;
 use Lunar\Models\Brand;
 
-class BrandsTable extends LunarTable
+class BrandsTable extends Component implements Tables\Contracts\HasTable
 {
     use Notifies;
+    use Tables\Concerns\InteractsWithTable;
 
     /**
      * {@inheritDoc}
      */
-    public bool $filterable = false;
-
-    /**
-     * {@inheritDoc}
-     */
-    public function build()
+    protected function getTableQuery(): Builder
     {
-        $this->tableBuilder->baseColumns([
-            ImageColumn::make('thumbnail', function ($record) {
-                if (! $thumbnail = $record->thumbnail) {
-                    return null;
-                }
-
-                return $thumbnail->getUrl('small');
-            })->heading(false),
-            TextColumn::make('name')->url(function ($record) {
-                return route('hub.brands.show', $record->id);
-            }),
-            TextColumn::make('products_count'),
-        ]);
+        return Brand::query();
     }
 
     /**
      * {@inheritDoc}
      */
-    public function getData()
+    protected function getTableColumns(): array
     {
-        return Brand::with(['thumbnail'])
-            ->withCount(['products'])
-            ->orderBy('name')
-            ->paginate($this->perPage);
+        return [
+            Tables\Columns\ImageColumn::make('thumbnail')
+                ->getStateUsing(function (Brand $record) {
+                    return $record->thumbnail->getUrl('small');
+                })
+                ->size(120)
+                ->label('Image'),
+            Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('products_count')
+                      ->getStateUsing(fn (Brand $record): string => $record->products->count() ?? '--'),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTableActions(): array
+    {
+        return [
+            Tables\Actions\ActionGroup::make([
+                Tables\Actions\RestoreAction::make(),
+                Tables\Actions\EditAction::make()->url(fn (Brand $record): string => route('hub.brands.show', ['brand' => $record])),
+            ]),
+        ];
+    }
+
+    /**
+     * Render the livewire component.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function render()
+    {
+        return view('adminhub::livewire.components.tables.base-table')
+            ->layout('adminhub::layouts.base');
     }
 }
