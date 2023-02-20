1c357cbc7c0383efba5e96dde9ea8fdd267da338
Adam Lee <adam@codelabx.ltd>
2023-02-20 15:56:49 +0000

    publishedProducts

--- src/Core/Models/Collection.php
+++ src/Core/Models/Collection.php
@@ -4,6 +4,7 @@ namespace Xtend\Extensions\Lunar\Core\Models;
 
 use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Lunar\Base\Casts\AsAttributeData;
+use Lunar\Models\Product;
 
 class Collection extends \Lunar\Models\Collection
 {
@@ -14,4 +15,9 @@ class Collection extends \Lunar\Models\Collection
         'attribute_data' => AsAttributeData::class,
         'legacy_data' => AsCollection::class,
     ];
+
+    public function publishedProducts()
+    {
+        return $this->products()->where('status', 'published');
+    }
 }
