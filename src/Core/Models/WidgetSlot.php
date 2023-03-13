b4c8698dbcae0e3ed4046a37de79b40be2f15790
Adam Lee <adam@codelabx.ltd>
2023-03-13 18:09:19 +0000

    Widgets relationship

--- src/Core/Models/WidgetSlot.php
+++ src/Core/Models/WidgetSlot.php
@@ -3,17 +3,25 @@
 namespace Xtend\Extensions\Lunar\Core\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class WidgetSlot extends Model
 {
     protected $table = 'xtend_builder_widget_slots';
 
+    protected $primaryKey = 'identifier';
+
     protected $casts = [
         'params' => 'array',
     ];
 
-    public function getRouteKeyName(): string
+    public function widgets(): BelongsToMany
     {
-        return 'identifier';
+        return $this->belongsToMany(
+            related: Widget::class,
+            table: 'xtend_builder_widget_slot_item',
+            foreignPivotKey: 'widget_slot_id',
+            relatedPivotKey: 'widget_id',
+        );
     }
 }
