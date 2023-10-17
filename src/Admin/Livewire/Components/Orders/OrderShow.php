4ed2a1242f350d7f403bdbcc1ee10694f3410d88
Adam Lee <adam@codelabx.ltd>
2023-10-17 17:11:09 +0100

    chore: Notify shipment

--- src/Admin/Livewire/Components/Orders/OrderShow.php
+++ src/Admin/Livewire/Components/Orders/OrderShow.php
@@ -2,134 +2,28 @@
 
 namespace Xtend\Extensions\Lunar\Admin\Livewire\Components\Orders;
 
-use Lunar\Base\Addressable;
 use Lunar\Hub\Http\Livewire\Components\Orders\OrderShow as LunarOrderShow;
-use Lunar\Models\Address;
-use Lunar\Models\OrderAddress;
 
 class OrderShow extends LunarOrderShow
 {
     /**
-     * Whether to show the discount panel.
-     *
-     * @var bool
+     * {@inheritDoc}
      */
-    public bool $showDiscount = false;
-
-    /**
-     * The instance of the shipping address.
-     *
-     * @var \Lunar\Models\OrderAddress
-     */
-    public ?OrderAddress $shippingAddress = null;
-
-    /**
-     * The instance of the shipping address.
-     *
-     * @var \Lunar\Models\OrderAddress
-     */
-    public ?OrderAddress $billingAddress = null;
-
-    /**
-     * Whether to show the address edit screen.
-     *
-     * @var bool
-     */
-    public bool $showShippingAddressEdit = false;
-
-    /**
-     * Whether to show the billing address edit.
-     *
-     * @var bool
-     */
-    public bool $showBillingAddressEdit = false;
-
-    /**
-     * @var int
-     */
-    public int $shippingAddressId = 0;
-
-    /**
-     * @var int
-     */
-    public int $billingAddressId = 0;
-
-    public function mount()
+    protected function getListeners()
     {
-        $this->shippingAddress = $this->order->shippingAddress ?: new OrderAddress();
-
-        $this->billingAddress = $this->order->billingAddress ?: new OrderAddress();
+        return array_merge(parent::getListeners(), [
+            'notifyShipmentWarning',
+            'notifyShipmentSuccess',
+        ]);
     }
 
-    /**
-     * Handler when shipping edit toggle is updated.
-     *
-     * @return void
-     */
-    public function updatedShowShippingAddressEdit()
+    public function notifyShipmentWarning()
     {
-        $this->shippingAddress = $this->shippingAddress->refresh();
+        $this->dispatchBrowserEvent('notifyShipmentWarning');
     }
 
-    /**
-     * Handler when billing address is changed.
-     *
-     * @return void
-     */
-    public function updatedBillingAddressId()
+    public function notifyShipmentSuccess()
     {
-        if (! $this->billingAddressId) {
-            return;
-        }
-
-        $address = Address::findOrFail($this->billingAddressId);
-        $this->order->billingAddress()->delete();
-        $this->billingAddress = $this->order->billingAddress()->create(
-            collect($address->toArray())->merge([
-                'address_id' => $address->id,
-                'type' => 'billing',
-            ])->toArray()
-        );
-    }
-
-    /**
-     * Handler when shipping address is changed.
-     *
-     * @return void
-     */
-    public function updatedShippingAddressId()
-    {
-        if (! $this->shippingAddressId) {
-            return;
-        }
-
-        $address = Address::findOrFail($this->shippingAddressId);
-        $this->order->shippingAddress()->delete();
-        $this->shippingAddress = $this->order->shippingAddress()->create(
-            collect($address->toArray())->merge([
-                'address_id' => $address->id,
-                'type' => 'shipping',
-            ])->toArray()
-        );
-    }
-
-    /**
-     * @return \Illuminate\Support\Collection
-     */
-    public function getAddressesOptionsProperty()
-    {
-        return $this->order->customer->addresses->mapWithKeys(function (Addressable $address) {
-            return [$address->id => $address->formatted];
-        });
-    }
-
-    /**
-     * Handle when a discount is successful.
-     *
-     * @return void
-     */
-    public function discountSuccess()
-    {
-        $this->showDiscount = false;
+        $this->dispatchBrowserEvent('notifyShipmentSuccess');
     }
 }
